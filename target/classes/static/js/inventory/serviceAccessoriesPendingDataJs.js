4b07bc59e574f767a80c8d6dd02aa3c6cf68f557
Saddam <01957987631a@gmail.com>
2025-09-29 22:19:24 +0600

    Remove Select access from purchase of coo

--- target/classes/static/js/inventory/serviceAccessoriesPendingDataJs.js
+++ target/classes/static/js/inventory/serviceAccessoriesPendingDataJs.js
@@ -233,7 +233,6 @@ window.initServiceAccessoriesPendingDataTable = function (allData, allAddData , 
 
                                             ${
 
-                                                solution.inventoryForPurchaseRequestStatus === null
                                                 ? `<input type="checkbox"
                                                       data-category="${solution.category}"
                                                       data-solution-name="${solution.name}"
